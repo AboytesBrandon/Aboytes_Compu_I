6f88923401a5fe69a92ea2326046cd8dcc093ccd
Brandon Aboytes <88738461+AboytesBrandon@users.noreply.github.com>
2022-11-13 11:20:52 -0600

    Subiendo archivos
    esta practica se entreg el domingo 13 de noviembre de 2022

---
+++ Puntos/Punto.cpp
@@ -0,0 +1,40 @@
+//
+// Created by ABOYTES on 11/4/2022.
+//
+
+#include "Punto.h"
+#include <math.h>
+
+Punto2D::Punto2D() {
+
+}
+
+float Punto2D::GetX() {
+    return x;
+}
+
+float Punto2D::GetY() {
+    return y;
+}
+
+void Punto2D::SetPosicion(float x_, float y_){
+    x = x_;
+    y = y_;
+}
+
+void Punto2D:: Trasladar(float tx, float ty){
+    x += tx;
+    y += ty;
+}
+
+void Punto2D:: RotarRespectoAlOrigen(float a){
+    a *= 3.14159/ 180.0;
+    float xp;
+    xp = x * cos (a) -  y * sin(a);
+    y = x * sin(a) + y * cos(a);
+    x = xp;
+}
+void Punto2D:: Escalar(float ex, float ey){
+    x *= ex;
+    y *= ey;
+}
